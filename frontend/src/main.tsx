b75d9338c00f15ff6a44e4a840ff59fe621213e6
P <30762620+p-hlp@users.noreply.github.com>
2024-02-05 14:54:45 +0100

    feat: add menubar with logout functionality (#97)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,15 +8,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { Auth0Provider, withAuthenticationRequired } from "@auth0/auth0-react";
+import { ThemeProvider } from "@emotion/react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { CssBaseline, createTheme } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { App } from "./App";
-import { AxiosProvider } from "./lib";
 import { ToastContainer } from "react-toastify";
+import { App } from "./App";
+import "./global.css";
+import { AxiosProvider } from "./lib";
 
 const rootRoute = createRootRoute();
 
@@ -39,6 +42,12 @@ const router = createRouter({
 
 export const queryClient = new QueryClient();
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Auth0Provider
@@ -51,15 +60,18 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     >
       <AxiosProvider>
         <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ToastContainer
-            theme="dark"
-            position="top-center"
-            closeOnClick
-            autoClose={3000}
-            hideProgressBar
-          />
-          <ReactQueryDevtools initialIsOpen={false} />
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <RouterProvider router={router} />
+            <ToastContainer
+              theme="dark"
+              position="top-center"
+              closeOnClick
+              autoClose={3000}
+              hideProgressBar
+            />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ThemeProvider>
         </QueryClientProvider>
       </AxiosProvider>
     </Auth0Provider>
