b9a6c436ddfb92c349e1fdee299a23f52dc774ca
p-hlp <ammunitionrec@gmail.com>
2024-03-10 19:25:52 +0100

    feat: show millisec in file details

--- frontend/src/shared/timeUtils.ts
+++ frontend/src/shared/timeUtils.ts
@@ -1,5 +1,7 @@
 export const formatDuration = (durationInSeconds: number): string => {
   const minutes = Math.floor(durationInSeconds / 60);
-  const seconds = Number((durationInSeconds % 60).toFixed(0));
-  return `${minutes}:${seconds.toString().padStart(2, "0")}min`;
+  const seconds = Math.floor(durationInSeconds % 60);
+  const milliseconds = Math.floor((durationInSeconds % 1) * 1000);
+  const timeFormat = `${minutes > 0 ? minutes + ":" : ""}${seconds.toString().padStart(2, "0")}:${milliseconds.toString().padStart(3, "0")}`;
+  return minutes > 0 ? `${timeFormat}min` : `${timeFormat}s`;
 };
